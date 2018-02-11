0c9ca9b1538629c0e2350e92ff3aad3ef6aa5de0
abonogofsky <bonogofskyamber@gmail.com>
2018-02-11 12:41:31 -0600

    Upload new portfolio

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+$.featherlightGallery.prototype.afterContent = function() {
+  var caption = this.$currentTarget.find('img').attr('alt');
+  this.$instance.find('.caption').remove();
+  $('<div class="caption">').text(caption).appendTo(this.$instance.find('.featherlight-content'));
+};
