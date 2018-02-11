f3b382dc6c1da3dd25402b6c0d1bf68af25d56c1
abonogofsky <bonogofskyamber@gmail.com>
2018-02-11 12:54:24 -0600

    Remove empty js file

--- js/scripts.js
+++
@@ -1,5 +0,0 @@
-$.featherlightGallery.prototype.afterContent = function() {
-  var caption = this.$currentTarget.find('img').attr('alt');
-  this.$instance.find('.caption').remove();
-  $('<div class="caption">').text(caption).appendTo(this.$instance.find('.featherlight-content'));
-};
